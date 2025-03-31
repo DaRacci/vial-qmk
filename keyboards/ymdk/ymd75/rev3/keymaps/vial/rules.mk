3c1c2e2ff3fd76b3e60c71a1e1418bf57ceb33d9
DaRacci <me@racci.dev>
2025-03-31 05:34:33 +0000

    feat: ymd75 v3 vial support

---
+++ keyboards/ymdk/ymd75/rev3/keymaps/vial/rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+
+LTO_ENABLE = yes
+COMBO_ENABLE = no
+MOUSEKEY_ENABLE = no
+MUSIC_ENABLE = no
+
+# I don't use RGB, may as well space the space.
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
